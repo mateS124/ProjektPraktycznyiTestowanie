df553dda1711235898e8b487dcd2336bab3a7c1a
mateS124 <113354697+mateS124@users.noreply.github.com>
2023-03-25 09:51:24 +0100

    first commit

---
+++ src/main/java/pl/sdacademy/projektpraktyczny/Main.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.projektpraktyczny;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String Test = "test";
+        System.out.println(Test);
+
+    }
+}
